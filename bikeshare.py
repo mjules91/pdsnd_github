f4f7f2c567eb73e5db5ef4e81f01ad99fe9fdb3e
JULES MUGABE <mugabejules91@gmail.com>
2021-10-15 12:07:41 +0200

    adding a call function

--- bikeshare.py
+++ bikeshare.py
@@ -216,7 +216,7 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+        address()
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
